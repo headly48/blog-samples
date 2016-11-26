24ebaabf50990fac1045a9f98c9a0e7da10709db
Joshua H <josh-48@hotmail.com>
2016-11-26 18:28:07 +1100

    update lorem-lipsum index.js

--- lorem-lipsum-lambda/index.js
+++ lorem-lipsum-lambda/index.js
@@ -1,9 +1,6 @@
 const loremIpsum = require('lorem-ipsum');
 
 exports.getLoremIpsum = function (event, context, callback) {
-	//getLoremIpsum
-	//console.log(loremIpsum());
-	console.log('Inside getLoremIpsum handler');
 
 
 	callback(null, {message: loremIpsum()});
