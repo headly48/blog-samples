466f1619595ada435592c09ccf025aab24eed2f3
Joshua Head <joshua.head@auspost.com.au>
2018-09-20 13:14:47 +1000

    - Adding ew s3-auth project (WIP)

---
+++ s3-authentication/.build/handler.ts
@@ -0,0 +1,19 @@
+import { Handler, Context, Callback, APIGatewayProxyEvent, APIGatewayEvent } from 'aws-lambda';
+
+
+const hello:Handler = async (event:APIGatewayProxyEvent, context:Context) => {
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+
+export { hello }
